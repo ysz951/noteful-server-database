c5cc95ec7d2ed8b8dc451bbedde421758860696a
ysz951 <shengyang_zhou@baylor.edu>
2020-08-03 03:22:44 -0500

    add patch folder

--- src/folders/folders-router.js
+++ src/folders/folders-router.js
@@ -86,4 +86,32 @@ foldersRouter
       })
       .catch(next)
   })
+  .patch(jsonParser, (req, res, next) => {
+    const { name } = req.body
+    const folderToUpdate = { name }
+
+    for (const [key, value] of Object.entries(folderToUpdate))
+      if (value == null)
+        return res.status(400).json({
+          error: { message: `Missing '${key}' in request body` }
+        })
+    const newName= formatName(name);
+    const errMessage = validateName(newName);
+    if (errMessage) {
+      return res.status(400).json({
+        error: { message: `${errMessage}` }
+      })
+    }
+    folderToUpdate.name = newName;
+    console.log(folderToUpdate)
+    FoldersService.updateFolder(
+      req.app.get('db'),
+      req.params.folder_id,
+      folderToUpdate
+    )
+      .then(numRowsAffected => {
+        res.status(204).end()
+      })
+      .catch(next)
+  })
 module.exports = foldersRouter
